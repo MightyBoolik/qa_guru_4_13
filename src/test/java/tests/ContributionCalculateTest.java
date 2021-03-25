e0f0afc751bdf63f2e1c4f3d8f2fdaf2a0d75343
MightyBoolik <bkuchaev@gmail.com>
2021-03-25 20:19:31 +0500

    Added allure steps for tests

--- src/test/java/tests/ContributionCalculateTest.java
+++ src/test/java/tests/ContributionCalculateTest.java
@@ -13,6 +13,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 
 @Tag("web")
@@ -24,23 +25,35 @@ public class ContributionCalculateTest extends TestBase {
     @Test
     @DisplayName("Check income from contribution")
     void percentContTest() {
-        open("");
-        $("#tab-32910").shouldHave(Condition.text("Рассчитайте свой кредит")).scrollTo();
-        $(".Slider__SliderBox-credit-calculator__dxxjdy-2").shouldBe(visible);
-        $(".Slider__Runner-credit-calculator__dxxjdy-5").hover();
-        slider.slider();
-        $(".ListTerms__Box-credit-calculator__sc-18rkehz-1").shouldBe(visible);
-        $(byText("5 лет")).click();
-        $(".main-pic__btn-wrap .scroll-to-service").click();
-        switchTo().window(1);
-        $(".top-block #step_1").shouldHave(text("Для получения кредита введите номер телефона"));
-        $("#OpenBkithreestepsForm_mobile").val(faker.phoneNumber().subscriberNumber(10));
-        $(byName("btn_submit_step_1")).click();
+        step("Open main page", () -> {
+                    open("");
+                    $("#tab-32910").shouldHave(Condition.text("Рассчитайте свой кредит")).scrollTo();
+                });
+        step("Set value in contribution calculator", () -> {
+                    $(".Slider__SliderBox-credit-calculator__dxxjdy-2").shouldBe(visible);
+                    $(".Slider__Runner-credit-calculator__dxxjdy-5").hover();
+                    slider.slider();
+                });
+        step("Set contribution time", () -> {
+                    $(".ListTerms__Box-credit-calculator__sc-18rkehz-1").shouldBe(visible);
+                    $(byText("5 лет")).click();
+                });
+        step("Send request button", () -> {
+                    $(".main-pic__btn-wrap .scroll-to-service").click();
+                    switchTo().window(1);
+                });
+        step("Input phone number", () -> {
+                    $(".top-block #step_1").shouldHave(text("Для получения кредита введите номер телефона"));
+                    $("#OpenBkithreestepsForm_mobile").val(faker.phoneNumber().subscriberNumber(10));
+                });
+        step("Click submit button", () -> {
+            $(byName("btn_submit_step_1")).click();
+        });
 
     }
     @Test
     @Story("Added story test")
-    @DisplayName("Main page should be loaded")
+    @DisplayName("Optional test for story")
     void WithStoryTest() {
         open("");
         $(".horizont__title").shouldBe(visible);
--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 @Tag("web")
 @Feature("Main page tests")
@@ -19,29 +20,46 @@ public class MainPageTest extends TestBase {
     @Test
     @DisplayName("Main page should be loaded")
     void PageLoadedTest() {
-        open("");
-        $(".horizont__title").shouldBe(visible);
+        step("Open main page", () -> {
+            open("");
+            $(".horizont__title").shouldBe(visible);
+        });
     }
 
     @Test
     @DisplayName("Internet-bank")
     void InternetBankButtonTest() {
-        open("");
-        sleep(5000);
-        $$("div.g-header_i .btn-internet-bank").find(text("Интернет-банк")).click();
-        $("div.popup-2__inner").shouldBe(visible);
-        $(byText("Google Play")).click();
+        step("Open main page", () -> {
+                    open("");
+                    sleep(5000);
+                });
+        step("Internet-bank button click", () -> {
+                    $$("div.g-header_i .btn-internet-bank").find(text("Интернет-банк")).click();
+                });
+        step("Google play service button click", () -> {
+            $("div.popup-2__inner").shouldBe(visible);
+            $(byText("Google Play")).click();
+        });
+
 
     }
 
     @Test
     @DisplayName("Test support chat")
     void phoneButtonTest() {
-        open("");
-        $("div.g-main").shouldBe(visible);
-        $("div.js-open-btn").click();
-        $(byText("Заказать звонок")).click();
-        $("#call-api-form .js-form-tel").val(faker.phoneNumber().subscriberNumber(9)).click();
+        step("Open main page", () -> {
+                    open("");
+                    $("div.g-main").shouldBe(visible);
+                });
+        step("Click phone button", () -> {
+                    $("div.js-open-btn").click();
+                });
+        step("Request a call button", () -> {
+                    $(byText("Заказать звонок")).click();
+                });
+        step("Input phone number", () -> {
+            $("#call-api-form .js-form-tel").val(faker.phoneNumber().subscriberNumber(9)).click();
+        });
 
     }
 
